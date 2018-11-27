ee3d024fc96fc0060ed3466c4fefbaa280cbf2e9
yuanandrei <45374738+yuanandrei@users.noreply.github.com>
2018-11-27 10:02:19 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+mga_daga = ["nathan","gelo","japet"]
+mga_numero = [1,2,3,4,5,6]
+mga_decimal_points = [1.0,1.2,1.3,1.4]
+
+ for x in range (len(mga_daga)):
+    print (mga_daga[x])
+ for y in range (len(mga_numero)):
+    print (mga_numero[y])
+ for z in range (len(mga_decimal_points)):
+     print(mga_decimal_points[z])
+print (len(mga_daga))
+print (len(mga_numero))
+print (len(mga_decimal_points))
